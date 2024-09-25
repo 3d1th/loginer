c87f89ad7149c2638d9aff2baf32badb55873fb4
3d1th <92995901+3d1th@users.noreply.github.com>
2024-09-25 21:46:04 +0900

    ㅁㅈㄷㄹ

--- main.go
+++ main.go
@@ -24,8 +24,6 @@ func main() {
 		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"),
 	)
 	ctx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
-
-	// defer cancel() 제거
 	ctx, _ = chromedp.NewContext(ctx)
 
 	loginURL := "https://discord.com/login"
